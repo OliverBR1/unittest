d16e0b2fa673340540d16316ad50ec744e93648c
OliverBR1 <Br.0liveira@outlook.com>
2026-09-08 16:15:04 -0300

    refactor UserServiceTest

--- src/test/java/tech/oliver/mockito/UserServiceTest.java
+++ src/test/java/tech/oliver/mockito/UserServiceTest.java
@@ -1,52 +1,113 @@
 package tech.oliver.mockito;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tech.oliver.ecommerce.Order;
+import tech.oliver.ecommerce.repository.OrderRepository;
+import tech.oliver.ecommerce.service.OrderService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class UserServiceTest {
+class OrderServiceTest {
 
     @Mock
-    private static Database database;
+    private OrderRepository repository;
 
-    @Spy
-    private static RealDatabase databaseSpy;
+    @Captor
+    private ArgumentCaptor<Order> orderArgumentCaptor;
 
     @InjectMocks
-    private static UserService userService;
+    private OrderService orderService;
 
-    @BeforeAll
-    public static void beforeAll() {
+    @Nested
+    class placeOrder {
+
+        @Test
+        @DisplayName("Should place order with success")
+        void shouldPlaceOrderWithSuccess() {
+
+            var dummyOrder = new Order(1, "Bruno", 200.0);
+
+            orderService.placeOrder(dummyOrder);
+
+            verify(repository, times(1)).save(orderArgumentCaptor.capture());
+            var orderCaptured = orderArgumentCaptor.getValue();
+            assertEqualsOrder(dummyOrder, orderCaptured);
+        }
+
+        @ParameterizedTest
+        @ValueSource(doubles = {0, -2.0, -50.0})
+        @DisplayName("Should throw exception when total is below or equal zero")
+        void shouldThrowExceptionWhenTotalIsBelowOrEqualZero(double total) {
+
+            var dummyOrder = new Order(1, "Bruno", total);
+
+            assertThrows(IllegalArgumentException.class, () -> {
+                orderService.placeOrder(dummyOrder);
+            });
+            verify(repository, times(0)).save(any());
+        }
+
+        @Test
+        @DisplayName("Should throw exception when place order")
+        void shouldThrowExceptionWhenPlaceOrder() {
+
+            var dummyOrder = new Order(1, "Bruno", 200.0);
+            doThrow(new RuntimeException()).when(repository).save(any());
+
+            assertThrows(RuntimeException.class, () -> {
+                orderService.placeOrder(dummyOrder);
+            });
+        }
     }
 
     @Nested
-    class getUserStatus {
+    class getOrder {
 
         @Test
-        @DisplayName("Should return an active user")
-        void shouldReturnAnActiveUser() {
-            // Arrange
-            int userId = 1;
-            String expectedStatus = "ACTIVE";
-            doReturn(expectedStatus).when(database).getStatus(eq(userId)); // test stub
+        @DisplayName("Should return order when exists")
+        void shouldReturnOrderWhenExists() {
 
-            // Act
-            var output = userService.getUserStatus(userId);
+            int orderId = 1;
+            var dummyOrder = new Order(1, "Bruno", 200.0);
+            doReturn(dummyOrder).when(repository).findById(eq(orderId));
 
-            // Assert
-            assertEquals(expectedStatus, output);
-            verify(database, times(1)).getStatus(eq(userId));
+            var order = orderService.getOrder(orderId);
+
+            assertNotNull(order);
+            assertEqualsOrder(dummyOrder, order);
+            verify(repository, times(1)).findById(eq(orderId));
         }
+
+        @Test
+        @DisplayName("Should return null when order does not exists")
+        void shouldReturnNullWhenOrderDoesNotExists() {
+
+            int orderId = 1;
+            doReturn(null).when(repository).findById(eq(orderId));
+
+            var order = orderService.getOrder(orderId);
+
+            assertNull(order);
+            verify(repository, times(1)).findById(eq(orderId));
+        }
+    }
+
+    private static void assertEqualsOrder(Order dummyOrder, Order order) {
+        assertEquals(dummyOrder.getId(), order.getId());
+        assertEquals(dummyOrder.getCustomer(), order.getCustomer());
+        assertEquals(dummyOrder.getTotal(), order.getTotal());
     }
 }
